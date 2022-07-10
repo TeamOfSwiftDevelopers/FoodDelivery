b85b02e3bc85a94e498e8f937cd9340cac0663b0
Pavel Mishanin <pavelmishanin@mail.ru>
2022-07-10 13:03:59 +0700

    main: initial commit

---
+++ FoodDelivery/FoodDelivery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodDelivery
+//
+//  Created by pavel mishanin on 10.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
